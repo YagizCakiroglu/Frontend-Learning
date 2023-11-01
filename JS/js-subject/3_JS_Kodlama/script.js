dcf9d4ce08b5c515a4e927673f7a3f68b46ea09c
Yağız Can Çakıroğlu <94487965+YagizCakiroglu@users.noreply.github.com>
2023-11-01 17:19:09 +0300

    Javasceipt starter  v64

---
+++ JS/js-subject/3_JS_Kodlama/script.js
@@ -0,0 +1,32 @@
+var urunAdi = 'İphone 13'; //string
+let urunFiyat = 15000; //number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+// let sayi1 = "10";
+// let sayi2 = "20";
+
+// console.log(Number(sayi1) + Number(sayi2));
+
+let sayi1 = 10;
+let sayi2 = 20;
+
+console.log(sayi1.toString() + sayi2.toString());
+
+let isim = "sena";
+let soyad = "özyılmaz";
+console.log(isim + ' ' + soyad);
+
+
+let sinavnotu = 70;
+let basarilimi = (sinavnotu >= 50)
+console.log(basarilimi);
+
+let yas;
+console.log(yas);
+console.log(typeof yas);
+
+yas = "";
+console.log(yas);
+console.log(typeof yas);
