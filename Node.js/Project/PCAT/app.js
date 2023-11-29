e3f34721706d42da24a9143e26741e17de717bb8
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2023-11-30 01:07:03 +0300

    node.js project v79-2

--- Node.js/Project/PCAT/app.js
+++ Node.js/Project/PCAT/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
+//Template Engine
+app.set("view engine", "ejs");
 
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+
+//ROUTES
 app.get('/', (req, res) => {
-
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description"
-  }
-  res.send(photo)
-})
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
 
 const port = 3000;
 app.listen(port, () => {
