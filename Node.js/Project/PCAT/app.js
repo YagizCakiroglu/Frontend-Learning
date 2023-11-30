861a0b9355e5daac89b73d2d0377d2d8568c1ef0
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2023-12-01 01:04:29 +0300

    node.js project v80

--- Node.js/Project/PCAT/app.js
+++ Node.js/Project/PCAT/app.js
@@ -1,25 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db')
+
+
 //Template Engine
 app.set("view engine", "ejs");
 
 
 //MIDDLEWARES
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about');
 });
 app.get('/add', (req, res) => {
-  res.render('add')
+  res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
