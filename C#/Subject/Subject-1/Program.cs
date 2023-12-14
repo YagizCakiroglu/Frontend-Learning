ffd8790b25caf967b951b8f0eb3eed016da8d4e5
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2023-12-15 02:03:25 +0300

    C# starter v84

---
+++ C#/Subject/Subject-1/Program.cs
@@ -0,0 +1,16 @@
+﻿ // Kısa ve uzun kenarı girilen dikdörtgenin alan ve çevresini hesaplayınız. 
+
+            Console.Write("kısa kenar: ");
+            int kisa = int.Parse(Console.ReadLine()); 
+
+            Console.Write("uzun kenar: ");
+            int uzun = int.Parse(Console.ReadLine());
+
+            var alan = kisa * uzun;
+            var cevre = (kisa + uzun)*2;
+
+            // var str = "Yağız Can";
+            // var price = 100.5;
+
+            // Console.WriteLine("alan: "+ alan + " çevre: "+ cevre);
+            Console.WriteLine($"alan: {alan} çevre: {cevre}");
