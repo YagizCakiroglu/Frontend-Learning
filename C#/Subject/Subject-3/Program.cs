bd14f7c88609454b572a4f64a60a9832a63413d3
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-01-02 14:44:39 +0300

    C# Hata yönetimi 1 V95-2

---
+++ C#/Subject/Subject-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp
+{   
+
+    class Product
+    {
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+           // Exception
+
+           // Exception 
+           // Unhandled exception. System.FormatException
+           // Unhandled exception. System.DivideByZeroException
+           // Unhandled exception. System.IndexOutOfRangeException
+           // Unhandled exception. System.NullReferenceException
+
+           Console.Write("1. sayı: ");
+           int sayi1 =int.Parse(Console.ReadLine());
+
+           Console.Write("2. sayı: ");
+           int sayi2 =int.Parse(Console.ReadLine());
+
+           var sonuc = sayi1 / sayi2;
+
+           int[] sayilar = new int[2];
+           // sayilar[2] = 10;
+
+           Product p = null;
+           Console.WriteLine(p.Name);
+
+           // Exception Handling
+           
+        }
+    }
+}
--- C#/Subject/Subject-3/obj/Debug/net8.0/Subject-3.AssemblyInfo.cs
+++ C#/Subject/Subject-3/obj/Debug/net8.0/Subject-3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Subject-1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Subject-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Subject-1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Subject-1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa247e17518b411b534bc1ba2225dbd676ef815a")]
+[assembly: System.Reflection.AssemblyProductAttribute("Subject-3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Subject-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
