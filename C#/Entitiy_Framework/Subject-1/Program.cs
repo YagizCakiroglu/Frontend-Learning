35f1e341bf38e24756ddce95961c8b0a8487fcc6
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-01-31 18:11:32 +0300

    C# Entitiy Framework Devam V101

--- C#/Entitiy_Framework/Subject-1/Program.cs
+++ C#/Entitiy_Framework/Subject-1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
 
 namespace ConsoleApp
 {
@@ -9,9 +12,13 @@ namespace ConsoleApp
         public DbSet<Product> Products {get; set;}
         public DbSet<Category> Categories {get; set;}
 
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => {builder.AddConsole(); });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=shop.db");
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlite("Data Source=shop.db");
         }
     }
 
@@ -35,7 +42,41 @@ namespace ConsoleApp
         
         static void Main(string[] args)
         {
-
+            AddProduct();
         }
+
+        static void AddProducts()
+        {
+            using (var db = new ShopContext())
+            {
+                var products = new List<Product>()
+                {
+                    new Product { Name = "İphone 11", Price=2000},
+                    new Product { Name = "İphone 12", Price=3000},
+                    new Product { Name = "İphone 13", Price=4000},
+                    new Product { Name = "İphone 14", Price=5000}
+                };
+
+                db.Products.AddRange(products);
+                db.SaveChanges();
+
+                Console.WriteLine("Veriler Eklendi");
+            }
+        }
+
+        static void AddProduct()
+        {
+            using (var db = new ShopContext())
+            {
+                var p = new Product { Name = "İphone 15", Price=8000};
+                
+
+                db.Products.Add(p);
+                db.SaveChanges();
+
+                Console.WriteLine("Veriler Eklendi");
+            }
+        }
+
     }
 }
--- C#/Entitiy_Framework/Subject-1/obj/Debug/net8.0/Subject-1.AssemblyInfo.cs
+++ C#/Entitiy_Framework/Subject-1/obj/Debug/net8.0/Subject-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Subject-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2dcce92264960cbb50e33806db4d187b65ce433")]
 [assembly: System.Reflection.AssemblyProductAttribute("Subject-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Subject-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
