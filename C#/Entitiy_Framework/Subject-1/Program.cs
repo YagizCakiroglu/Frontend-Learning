0abadb189582803ac9d80e494494443f4cf8e4d7
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-02-01 18:05:23 +0300

    C# Entitiy Framework Devam V102

--- C#/Entitiy_Framework/Subject-1/Program.cs
+++ C#/Entitiy_Framework/Subject-1/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-
 
 namespace ConsoleApp
-{
+{     
     public class ShopContext: DbContext
-    {
-        public DbSet<Product> Products {get; set;}
-        public DbSet<Category> Categories {get; set;}
+    {        
+        public DbSet<Product> Products {get;set;}
+        public DbSet<Category> Categories { get; set; }
 
-        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => {builder.AddConsole(); });
-
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });   
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
@@ -23,13 +23,15 @@ namespace ConsoleApp
     }
 
     public class Product
-    {
-        //primary key(Id, <table_name>Id)
+    { 
         public int Id { get; set; }
+       
         [MaxLength(100)]
         [Required]
         public string Name { get; set; }
+
         public decimal Price { get; set; }
+
     }
 
     public class Category
@@ -37,46 +39,173 @@ namespace ConsoleApp
         public int Id { get; set; }
         public string Name { get; set; }
     }
+
     class Program
     {
-        
         static void Main(string[] args)
         {
-            AddProduct();
+            DeleteProduct(5);
         }
 
+        static void DeleteProduct(int id)
+        {
+            using(var db = new ShopContext())
+            {
+
+                var p = new Product(){Id=5};
+
+                // db.Products.Remove(p);
+                db.Entry(p).State = EntityState.Deleted;
+                db.SaveChanges();
+
+                // var p = db.Products.FirstOrDefault(i=>i.Id==id);
+
+                // if (p!=null)
+                // {
+                //     db.Products.Remove(p);
+                //     db.SaveChanges();
+
+                //     Console.WriteLine("Veri Silindi");
+                // }
+            }
+        }
+        static void UpdateProduct()
+        {
+
+            using(var db = new ShopContext())
+            {
+                var p = db.Products.Where(i=>i.Id==1).FirstOrDefault();
+
+                if(p!=null)
+                {
+                    p.Price = 2400;
+                    
+                    db.Products.Update(p);
+                    db.SaveChanges();
+                }
+            }
+
+            // using(var db = new ShopContext())
+            // {
+            //     var entity = new Product(){Id=1};
+                
+            //     db.Products.Attach(entity);
+
+            //     entity.Price = 3000;
+
+            //     db.SaveChanges();
+            // }   
+
+
+
+            //   using(var db = new ShopContext())
+            //   {
+            //       // change tracking
+            //       var p = db
+            //                 .Products
+            //                 //.AsNoTracking()
+            //                 .Where(i=>i.Id==1)
+            //                 .FirstOrDefault();
+
+            //       if (p!=null)
+            //       {
+            //             p.Price *= 1.2m;
+            //             db.SaveChanges();
+            //             Console.WriteLine("güncelleme yapıldı.");
+
+            //       }
+            //   }  
+        }
+        static void GetProductByName(string name)
+        {
+            using(var context = new ShopContext())
+            {
+                var products = context
+                                .Products
+                                .Where(p => p.Name.ToLower().Contains(name.ToLower()))
+                                .Select(p => 
+                                        new {
+                                            p.Name,
+                                            p.Price
+                                        })
+                                .ToList();
+             
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"name: {p.Name} price: {p.Price}");
+                }
+                
+            }
+        }
+        static void GetProductById(int id)
+        {
+            using(var context = new ShopContext())
+            {
+                var result = context
+                                .Products
+                                .Where(p => p.Id == id)
+                                .Select(p => 
+                                        new {
+                                            p.Name,
+                                            p.Price
+                                        })
+                                .FirstOrDefault();
+             
+
+                
+                Console.WriteLine($"name: {result.Name} price: {result.Price}");
+                
+            }
+        }
+        static void GetAllProducts()
+        {
+            using(var context = new ShopContext())
+            {
+                var products = context
+                .Products
+                .Select(p => 
+                    new {
+                        p.Name,
+                        p.Price
+                    })
+                .ToList();
+
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"name: {p.Name} price: {p.Price}");
+                }
+            }
+        }
         static void AddProducts()
         {
-            using (var db = new ShopContext())
+            using(var db = new ShopContext())
             {
                 var products = new List<Product>()
                 {
-                    new Product { Name = "İphone 11", Price=2000},
-                    new Product { Name = "İphone 12", Price=3000},
-                    new Product { Name = "İphone 13", Price=4000},
-                    new Product { Name = "İphone 14", Price=5000}
-                };
+                    new Product { Name = "İphone 11", Price=3000 },
+                    new Product { Name = "İphone 12", Price=4000 },
+                    new Product { Name = "İphone 13", Price=5000 },
+                    new Product { Name = "İphone 14", Price=6000 }
+                };          
 
-                db.Products.AddRange(products);
+                db.Products.AddRange(products);                
                 db.SaveChanges();
 
-                Console.WriteLine("Veriler Eklendi");
+                Console.WriteLine("veriler eklendi.");
             }
         }
-
         static void AddProduct()
         {
-            using (var db = new ShopContext())
+            using(var db = new ShopContext())
             {
-                var p = new Product { Name = "İphone 15", Price=8000};
-                
+                var p = new Product { Name = "İphone 15", Price=8000 }; 
 
-                db.Products.Add(p);
+                db.Products.Add(p);                
                 db.SaveChanges();
 
-                Console.WriteLine("Veriler Eklendi");
+                Console.WriteLine("veriler eklendi.");
             }
         }
-
+   
     }
 }
