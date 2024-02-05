15eac4450ff203f7d7ea2305c4be6cccda4cd80c
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-02-06 01:00:18 +0300

    C# Entitiy Framework Devam V104

--- C#/Entitiy_Framework/Subject-1/Migrations/20240205215240_InitialCreate.Designer.cs
+++ C#/Entitiy_Framework/Subject-1/Migrations/20240205215240_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using ConsoleApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Subject_1.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20240205215240_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
--- C#/Entitiy_Framework/Subject-1/Program.cs
+++ C#/Entitiy_Framework/Subject-1/Program.cs
@@ -12,7 +12,11 @@ namespace ConsoleApp
         public DbSet<Product> Products {get;set;}
         public DbSet<Category> Categories { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
+        // public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+
 
         public static readonly ILoggerFactory MyLoggerFactory
             = LoggerFactory.Create(builder => { builder.AddConsole(); });   
@@ -35,6 +39,8 @@ namespace ConsoleApp
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+
+        public List<Address> Addresses { get; set; } //Navigation Property
     }
 
     public class Address
@@ -44,7 +50,8 @@ namespace ConsoleApp
         public string Title { get; set; }
         public string Body { get; set; }
 
-        public int UserId { get; set; }
+        public User User { get; set; } // navigation property
+        public int UserId { get; set; } // int=> null, 1, 2, 3, 4
     }
     public class Product
     { 
@@ -66,177 +73,77 @@ namespace ConsoleApp
         public string Name { get; set; }
     }
 
-    public class Order
-    {
-        public int Id { get; set; }
-        public int ProductId { get; set; }
-        public DateTime DateAdded { get; set; }
-    }
+    // public class Order
+    // {
+    //     public int Id { get; set; }
+    //     public int ProductId { get; set; }
+    //     public DateTime DateAdded { get; set; }
+    // }
     class Program
     {
         static void Main(string[] args)
         {
-            AddProducts();
-        }
+            //InsertUsers();
+            //InsertAddresses();
 
-        static void DeleteProduct(int id)
-        {
             using(var db = new ShopContext())
             {
+                var user = db.Users.FirstOrDefault(i=>i.Username=="yagizcakiroglu");
 
-                var p = new Product(){Id=6};
-
-                // db.Products.Remove(p);
-                db.Entry(p).State = EntityState.Deleted;
-                db.SaveChanges();    
-
-                // var p = db.Products.FirstOrDefault(i=>i.Id==id);
-
-                // if (p!=null)
-                // {
-                //     db.Products.Remove(p);
-                //     db.SaveChanges();
-
-                //     Console.WriteLine("veri silindi");
-                // }
-            }
-        }
-        static void UpdateProduct()
-        {
-            using(var db = new ShopContext())
-            {
-                var p = db.Products.Where(i=>i.Id==1).FirstOrDefault();
-
-                if(p!=null)
+                if(user!=null)
                 {
-                    p.Price = 2400;
+                    user.Addresses = new List<Address>();
 
-                    db.Products.Update(p);
+                    user.Addresses.AddRange(
+                        new List<Address>(){
+                            new Address(){Fullname="Yağız Çakıroğlu",Title="İş adresi 1",Body="İstanbul"},
+                            new Address(){Fullname="Yağız Çakıroğlu",Title="İş adresi 2 ",Body="İstanbul"},
+                            new Address(){Fullname="Yağız Çakıroğlu",Title="İş adresi 3",Body="İstanbul"}
+                        }
+                    );
+
                     db.SaveChanges();
                 }
             }
 
-            // using(var db = new ShopContext())
-            // {
-            //     var entity = new Product(){Id=1};
-                
-            //     db.Products.Attach(entity);
-
-            //     entity.Price = 3000;
-
-            //     db.SaveChanges();
-            // }   
-
-
-
-            //   using(var db = new ShopContext())
-            //   {
-            //       // change tracking
-            //       var p = db
-            //                 .Products
-            //                 //.AsNoTracking()
-            //                 .Where(i=>i.Id==1)
-            //                 .FirstOrDefault();
-
-            //       if (p!=null)
-            //       {
-            //             p.Price *= 1.2m;
-            //             db.SaveChanges();
-            //             Console.WriteLine("güncelleme yapıldı.");
-
-            //       }
-            //   }  
+            
+            
         }
-        static void GetProductByName(string name)
-        {
-            using(var context = new ShopContext())
-            {
-                var products = context
-                                .Products
-                                .Where(p => p.Name.ToLower().Contains(name.ToLower()))
-                                .Select(p => 
-                                        new {
-                                            p.Name,
-                                            p.Price
-                                        })
-                                .ToList();
-             
-                foreach (var p in products)
-                {
-                    Console.WriteLine($"name: {p.Name} price: {p.Price}");
-                }
-                
-            }
-        }
-        static void GetProductById(int id)
-        {
-            using(var context = new ShopContext())
-            {
-                var result = context
-                                .Products
-                                .Where(p => p.Id == id)
-                                .Select(p => 
-                                        new {
-                                            p.Name,
-                                            p.Price
-                                        })
-                                .FirstOrDefault();
-             
 
-                
-                Console.WriteLine($"name: {result.Name} price: {result.Price}");
-                
-            }
-        }
-        static void GetAllProducts()
+        static void InsertUsers()
         {
-            using(var context = new ShopContext())
-            {
-                var products = context
-                .Products
-                .Select(p => 
-                    new {
-                        p.Name,
-                        p.Price
-                    })
-                .ToList();
+            var users = new List<User>(){
+                new User(){Username="yagizcakiroglu",Email="info@yagizcakiroglu.com"},
+                new User(){Username="yusufcakiroglu",Email="info@yusufcakiroglu.com"},
+                new User(){Username="mehmetcakiroglu",Email="info@mehmetcakiroglu.com"},
+                new User(){Username="tugbacakiroglu",Email="info@tugbacakiroglu.com"},
+            };
 
-                foreach (var p in products)
-                {
-                    Console.WriteLine($"name: {p.Name} price: {p.Price}");
-                }
-            }
-        }
-        static void AddProducts()
-        {
             using(var db = new ShopContext())
             {
-                var products = new List<Product>()
-                {
-                    new Product { Name = "Samsung S6", Price=3000 },
-                    new Product { Name = "Samsung S7", Price=4000 },
-                    new Product { Name = "Samsung S8", Price=5000 },
-                    new Product { Name = "Samsung S9", Price=6000 }
-                };          
-
-                db.Products.AddRange(products);                
+                db.Users.AddRange(users);
                 db.SaveChanges();
-
-                Console.WriteLine("veriler eklendi.");
             }
         }
-        static void AddProduct()
+
+        static void InsertAddresses()
         {
+            var addresses = new List<Address>(){
+                new Address(){Fullname="Yağız Çakıroğlu",Title="Ev adresi",Body="İstanbul",UserId=1},
+                new Address(){Fullname="Yağız Çakıroğlu",Title="İş adresi",Body="İstanbul",UserId=1},
+                new Address(){Fullname="Yusuf Çakıroğlu",Title="Ev adresi",Body="İstanbul",UserId=3},
+                new Address(){Fullname="Mehmet Çakıroğlu",Title="İş adresi",Body="İstanbul",UserId=3},
+                new Address(){Fullname="Tuğba Çakıroğlu",Title="İş adresi",Body="İstanbul",UserId=2},
+                new Address(){Fullname="Tosun Çakıroğlu",Title="İş adresi",Body="İstanbul",UserId=4}
+             
+            };
+
             using(var db = new ShopContext())
             {
-                var p = new Product { Name = "Samsung S10", Price=8000 }; 
-
-                db.Products.Add(p);                
+                db.Addresses.AddRange(addresses);
                 db.SaveChanges();
-
-                Console.WriteLine("veriler eklendi.");
             }
         }
-   
+
     }
 }
