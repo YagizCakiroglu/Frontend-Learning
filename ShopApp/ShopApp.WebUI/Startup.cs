d832c59c1c63ae44d3d1e3a86677cbef7a75cffd
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-02-20 23:28:40 +0300

    C# MVC ShopApp Temel Özellikler 5

--- ShopApp/ShopApp.WebUI/Startup.cs
+++ ShopApp/ShopApp.WebUI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace shopapp.webui
@@ -24,6 +25,17 @@ namespace shopapp.webui
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseStaticFiles(); // wwwroot
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),"node_modules")),
+                    RequestPath="/modules"                
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
